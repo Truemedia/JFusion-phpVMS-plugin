59c8b90cb0872ba14592e8a69f06d157025b7700
Wade Penistone <truemedia@mediacityonline.net>
2011-08-29 16:09:38 +0100

    used code addition to check if user logged in phpvms in or with blank session

--- codeaddition.php
+++ codeaddition.php
@@ -1,14 +1,4 @@
 <?php
-define( '_JEXEC', 1 );
-define( 'DS', DIRECTORY_SEPARATOR );
-define('JPATH_BASE', dirname(__FILE__) );
-
-require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-
-$mainframe =& JFactory::getApplication('site');
-$mainframe->initialise();
-
-$session =& JFactory::getSession();
-$user = $session->get( 'user' );
+$logcheck = SessionManager::Get('loggedin');
+if(self::$session_id == '' || $logcheck == false)
 ?>
